66607da83d01d7af03273597c39aa951c23e2173
Jon Guan <jon@scanadu.com>
2014-09-25 14:14:16 -0700

    assignment 1 addition

---
+++ app/controllers/Application.scala
@@ -0,0 +1,286 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+import play.api.libs.iteratee.Enumerator
+import domain.user._
+import org.joda.time.{DateTime, DateTimeZone}
+import org.joda.time.format.ISODateTimeFormat
+import org.joda.time.format.DateTimeFormat
+
+object Application extends Controller {
+
+  def index = Action (parse.json) {
+    //Ok(views.html.index("Your new application is ready."))
+    implicit request => Ok("Got request [" + request + "]")
+  }
+  
+implicit val userIDWrites: Writes[Int] = new Writes[Int] {
+    def writes(i:Int): JsValue = {
+        return JsString("u-"+i.toString)
+    }
+}
+
+implicit val cardIDWrites: Writes[Int] = new Writes[Int] {
+    def writes(i:Int): JsValue = {
+        return JsString("c-"+i.toString)
+    }
+}
+
+implicit val webIDWrites: Writes[Int] = new Writes[Int] {
+    def writes(i:Int): JsValue = {
+        return JsString("l-"+i.toString)
+    }
+}
+
+implicit val bankIDWrites: Writes[Int] = new Writes[Int] {
+    def writes(i:Int): JsValue = {
+        return JsString("b-"+i.toString)
+    }
+}
+
+private lazy val ISODateTimeFormatter = ISODateTimeFormat.dateTime.withZone(DateTimeZone.UTC)
+private lazy val ISODateTimeParser = ISODateTimeFormat.dateTimeParser
+
+implicit val cardJodaDateReads = Reads.jodaDateReads("MM-dd-yyyy")
+implicit val cardJodaDateWrites = Writes.jodaDateWrites("MM-dd-yyyy")
+  
+implicit val DateTimeFormatter = new Format[DateTime] {
+    def reads(j: JsValue) = JsSuccess(ISODateTimeParser.parseDateTime(j.as[String]))
+    def writes(o: DateTime): JsValue = JsString(ISODateTimeFormatter.print(o))
+  } 
+  
+implicit val jodaTimeWrites: Writes[DateTime] = new Writes[DateTime] {
+    def writes(c: DateTime): JsValue = {
+      return DateTimeFormatter.writes(c)
+    }
+  }
+  
+  // Read obj -> write JSON
+implicit val userWrites: Writes[User] = (
+    (JsPath \ "user_id").writeNullable[Int](userIDWrites) and
+    (JsPath \ "email").write[String] and
+    (JsPath \ "password").write[String] and
+    (JsPath \ "name").writeNullable[String] and
+    (JsPath \ "created_at").writeNullable[DateTime](jodaTimeWrites) and
+    (JsPath \ "updated_at").writeNullable[DateTime](jodaTimeWrites)
+)(unlift(User.unapply))
+
+implicit val cardWrites: Writes[IDCard] = (
+    (JsPath \ "card_id").writeNullable[Int](cardIDWrites) and
+    (JsPath \ "card_name").write[String] and
+    (JsPath \ "card_number").write[String] and
+    (JsPath \ "expiration_date").writeNullable[DateTime](cardJodaDateWrites)
+)(unlift(IDCard.unapply))
+
+implicit val webWrites: Writes[WebLogin] = (
+    (JsPath \ "login_id").writeNullable[Int](webIDWrites) and
+    (JsPath \ "url").write[String] and
+    (JsPath \ "login").write[String] and
+    (JsPath \ "password").write[String] 
+)(unlift(WebLogin.unapply))  
+    
+implicit val bankWrites: Writes[BankAccount] = (
+    (JsPath \ "ba_id").writeNullable[Int](bankIDWrites) and
+    (JsPath \ "account_name").writeNullable[String] and
+    (JsPath \ "routing_number").write[String] and
+    (JsPath \ "account_number").write[String] 
+)(unlift(BankAccount.unapply))
+
+// Input JSON, output Object
+implicit val userReads: Reads[User] = (
+    (JsPath \ "user_id").readNullable[Int] and
+    (JsPath \ "email").read[String] and
+    (JsPath \ "password").read[String] and
+    (JsPath \ "name").readNullable[String] and
+    (JsPath \ "created_at").readNullable[DateTime] and
+    (JsPath \ "updated_at").readNullable[DateTime] 
+)(User.apply _)
+
+implicit val cardReads: Reads[IDCard] = (
+    (JsPath \ "card_id").readNullable[Int] and
+    (JsPath \ "card_name").read[String] and
+    (JsPath \ "card_number").read[String] and
+    (JsPath \ "expiration_date").readNullable[DateTime](cardJodaDateReads)
+)(IDCard.apply _)
+
+implicit val webReads: Reads[WebLogin] = (
+    (JsPath \ "login_id").readNullable[Int] and
+    (JsPath \ "url").read[String] and
+    (JsPath \ "login").read[String] and
+    (JsPath \ "password").read[String] 
+)(WebLogin.apply _)
+
+implicit val bankReads: Reads[BankAccount] = (
+    (JsPath \ "ba_id").readNullable[Int] and
+    (JsPath \ "account_name").readNullable[String] and
+    (JsPath \ "routing_number").read[String] and
+    (JsPath \ "account_number").read[String] 
+)(BankAccount.apply _)
+  
+  // Utility functions
+  def getIntIdValue(string_id:String): Int = {
+      return string_id.drop(2).toInt
+  }
+  
+  // User
+  def getUser(user_id:String) = Action { request =>
+    val modifiedSince:DateTime = DateTime.parse(request.headers.get("If-Modified-Since").getOrElse("1970-01-01T00:00:00.000Z"))
+    val user:User = User.getUser(getIntIdValue(user_id)).getOrElse(null)
+    val lastModified:DateTime = user.updated_at.getOrElse(user.created_at.getOrElse(new DateTime()))
+    if  (modifiedSince.compareTo(lastModified) >= 0) {
+        NotModified
+    } else {
+        val json = Json.toJson(user)
+        Ok(json).withHeaders(
+            LAST_MODIFIED -> DateTimeFormatter.writes(lastModified).toString()
+            )
+    }
+    
+  }
+  
+  def updateUser (user_id:String) = Action(BodyParsers.parse.json) { request =>
+    val userResult = request.body.validate[User]
+    userResult.fold(
+    errors => {
+        Result(
+            header = ResponseHeader(400, Map(CONTENT_TYPE->"application/json")),
+            body = Enumerator(Json.obj("status" ->"KO", "message" -> JsError.toFlatJson(errors)).toString.getBytes())
+            )
+      //BadRequest(Json.obj("status" ->"KO", "message" -> JsError.toFlatJson(errors)))
+    },
+    user => { 
+        val updatedUser = User.updateUser(getIntIdValue(user_id), user)
+      Result(
+          header = ResponseHeader(201, Map(CONTENT_TYPE->"application/json")),
+          body = Enumerator(Json.toJson(updatedUser).toString().getBytes())
+          )
+    }
+  )}
+  
+  def listUsers = Action {
+    val json = Json.toJson(User.userHashMap.values.toList)
+    Ok(json)
+}
+  
+def createUser = Action(BodyParsers.parse.json) { request =>
+  val userResult = request.body.validate[User]
+  userResult.fold(
+    errors => {
+        Result(
+            header = ResponseHeader(400, Map(CONTENT_TYPE->"application/json")),
+            body = Enumerator(Json.obj("status" ->"KO", "message" -> JsError.toFlatJson(errors)).toString.getBytes())
+            )
+      //BadRequest(Json.obj("status" ->"KO", "message" -> JsError.toFlatJson(errors)))
+    },
+    user => { 
+      val savedUser = User.save(user)
+      Result(
+          header = ResponseHeader(201, Map(CONTENT_TYPE->"application/json")),
+          body = Enumerator(Json.toJson(savedUser).toString().getBytes())
+          )
+    }
+  )
+}
+
+def createCard(user_id:String) = Action(BodyParsers.parse.json) { request =>
+   val cardResult = request.body.validate[IDCard]
+   cardResult.fold(
+    errors => {
+        Result(
+            header = ResponseHeader(400, Map(CONTENT_TYPE->"application/json")),
+            body = Enumerator(Json.obj("status" ->"KO", "message" -> JsError.toFlatJson(errors)).toString.getBytes())
+            )
+      //BadRequest(Json.obj("status" ->"KO", "message" -> JsError.toFlatJson(errors)))
+    },
+    card => { 
+      val savedCard = User.createCard(getIntIdValue(user_id), card)
+      Result(
+          header = ResponseHeader(201, Map(CONTENT_TYPE->"application/json")),
+          body = Enumerator(Json.toJson(savedCard).toString().getBytes())
+          )
+    }
+  )
+}
+
+def getCards(user_id:String) = Action { request =>
+    val json = Json.toJson(User.getCards(getIntIdValue(user_id)))
+    Ok(json)
+}
+
+def getCard(user_id:String, card_id:String) = Action { request =>
+    val json = Json.toJson(User.getCard(getIntIdValue(user_id), getIntIdValue(card_id)))
+    Ok(json)
+}
+
+def deleteCard(user_id:String, card_id:String) = Action { request =>
+    User.deleteCard(getIntIdValue(user_id), getIntIdValue(card_id))
+    NoContent
+}
+
+
+// Web Logins
+def createLogin(user_id:String) = Action(parse.json) { request =>
+    val webResult = request.body.validate[WebLogin]
+    webResult.fold(
+    errors => {
+        Result(
+            header = ResponseHeader(400, Map(CONTENT_TYPE->"application/json")),
+            body = Enumerator(Json.obj("status" ->"KO", "message" -> JsError.toFlatJson(errors)).toString.getBytes())
+            )
+      //BadRequest(Json.obj("status" ->"KO", "message" -> JsError.toFlatJson(errors)))
+    },
+    login => { 
+      val savedLogin = User.createLogin(getIntIdValue(user_id), login)
+      Result(
+          header = ResponseHeader(201, Map(CONTENT_TYPE->"application/json")),
+          body = Enumerator(Json.toJson(savedLogin).toString().getBytes())
+          )
+    }
+  )
+}
+
+def getLogins(user_id:String) = Action { request =>
+    val json = Json.toJson(User.getLogins(getIntIdValue(user_id)))
+    Ok(json)
+}
+
+def deleteLogin(user_id:String, login_id:String) = Action { request =>
+    User.deleteLogin(getIntIdValue(user_id), getIntIdValue(login_id))
+    NoContent
+}
+
+// Bank Accounts
+def createBankAccount(user_id:String) = Action(parse.json) { request =>
+    val bankResult = request.body.validate[BankAccount]
+    bankResult.fold(
+    errors => {
+        Result(
+            header = ResponseHeader(400, Map(CONTENT_TYPE->"application/json")),
+            body = Enumerator(Json.obj("status" ->"KO", "message" -> JsError.toFlatJson(errors)).toString.getBytes())
+            )
+      //BadRequest(Json.obj("status" ->"KO", "message" -> JsError.toFlatJson(errors)))
+    },
+    bank => { 
+      val savedBank = User.createBankAccount(getIntIdValue(user_id), bank)
+      Result(
+          header = ResponseHeader(201, Map(CONTENT_TYPE->"application/json")),
+          body = Enumerator(Json.toJson(savedBank).toString().getBytes())
+          )
+    }
+  )
+}
+
+def getBankAccounts(user_id:String) = Action { request =>
+   val json = Json.toJson(User.getBankAccounts(getIntIdValue(user_id)))
+    Ok(json)
+}
+def deleteBankAccount(user_id:String, ba_id:String) = Action { request =>
+   User.deleteBankAccount(getIntIdValue(user_id), getIntIdValue(ba_id))
+    NoContent
+}
+
+
+}
---
+++ app/controllers/User.scala
@@ -0,0 +1,206 @@
+package domain.user
+
+import org.joda.time._
+import java.util.Date
+import java.text.SimpleDateFormat
+import org.joda.time.{DateTime, DateTimeZone}
+import org.joda.time.format.ISODateTimeFormat
+import scala.util.Random._
+import scala.collection.mutable.HashMap
+
+case class User(user_id:Option[Int], email: String, password: String, name:Option[String], created_at:Option[DateTime], updated_at:Option[DateTime])
+case class IDCard(card_id:Option[Int], card_name:String, card_number:String, expiration_date:Option[DateTime])
+case class WebLogin(login_id:Option[Int], url:String, login:String, password:String)
+case class BankAccount(ba_id:Option[Int], account_name:Option[String], routing_number:String, account_number:String)
+
+object User {
+    
+    var userHashMap = scala.collection.mutable.HashMap[Int, User]()
+    // Hash maps of hash maps {user_id: hashMap} --> {card_id:card}
+    var cardHashMap = scala.collection.mutable.HashMap[Int, HashMap[Int, IDCard]]()
+    var webHashMap = scala.collection.mutable.HashMap[Int, HashMap[Int, WebLogin]]()
+    var bankHashMap = scala.collection.mutable.HashMap[Int, HashMap[Int, BankAccount]]()
+    
+  var list: List[User] = {
+    List(
+      User(
+        Some(1),
+        "abc@abc.com",
+        "pwd123",
+        Some("abc"),
+        Some(new DateTime()),
+        None
+        
+      ),
+      User(
+         Some(2),
+        "xyz@xyz.com",
+        "pass456",
+        None,
+        Some(new DateTime()),
+        Some(new DateTime())
+      )
+    )
+  }
+    
+  def save(user: User): User = {
+      var newUser:User = null;
+      val user_id = user.user_id.getOrElse(-1)
+      if (user_id == -1) {
+          // New User
+          val new_id:Int = scala.util.Random.nextInt(10)
+          newUser = user.copy(user_id=Some(new_id), created_at=Some(new DateTime()))
+          
+      } else {
+          // Update the updated_at
+          newUser = user.copy(updated_at=Some(new DateTime()))
+      }
+    //list = list ::: List(newUser)
+    userHashMap.put(newUser.user_id.getOrElse(-1), newUser)
+    return newUser
+  }
+  
+  def getUser(user_id:Int)  = {
+      userHashMap.get(user_id)
+  }
+  
+  def updateUser(user_id:Int, bodyUser:User): User = {
+      val dbUser:User = userHashMap.get(user_id).getOrElse(null)
+      if (dbUser == null) {
+          return null
+      }
+      var newUser:User = bodyUser.copy(user_id=dbUser.user_id, created_at=dbUser.created_at, updated_at=Some(new DateTime()))
+      userHashMap.put(newUser.user_id.getOrElse(-1), newUser)
+      newUser
+  }
+  
+  
+  // Cards
+  def createCard(user_id:Int, card:IDCard): IDCard = {
+      var userCardHashMap = cardHashMap.get(user_id).getOrElse(null)
+      // Create hash map if necessary
+      if (userCardHashMap == null) {
+          userCardHashMap = HashMap[Int, IDCard]()
+          cardHashMap.put(user_id, userCardHashMap);
+      }
+      
+      // Validate card
+      var newCard:IDCard = null;
+      val card_id = card.card_id.getOrElse(-1)
+      if (card_id == -1) {
+          // New User
+          val newCard_id = scala.util.Random.nextInt(10)
+          newCard = card.copy(card_id = Some(newCard_id))
+          userCardHashMap.put(newCard.card_id.getOrElse(-1), newCard)
+          return newCard
+      } else {
+          // error
+          return null
+      }
+  }
+  
+  def getCards(user_id:Int) :List[IDCard] = {
+      var userCardHashMap = cardHashMap.get(user_id).getOrElse(null)
+      if (userCardHashMap == null) {
+          List()
+      } else {
+          userCardHashMap.values.toList
+      }
+  }
+  
+  def getCard(user_id:Int, card_id:Int): IDCard = {
+      var userCardHashMap = cardHashMap.get(user_id).getOrElse(null)
+      if (userCardHashMap == null) {
+          return null
+      } else {
+          userCardHashMap.get(card_id).getOrElse(null)
+      }
+  }
+  
+  def deleteCard(user_id:Int, card_id:Int) = {
+      var userCardHashMap = cardHashMap.get(user_id).getOrElse(null)
+      if (userCardHashMap != null) {
+          userCardHashMap.remove(card_id)
+      }
+  }
+  
+  // Web Login
+    def createLogin(user_id:Int, login:WebLogin): WebLogin = {
+      var userWebHashMap = webHashMap.get(user_id).getOrElse(null)
+      // Create hash map if necessary
+      if (userWebHashMap == null) {
+          userWebHashMap = HashMap[Int, WebLogin]()
+          webHashMap.put(user_id, userWebHashMap);
+      }
+      
+      // Validate 
+      var newLogin:WebLogin = null;
+      val login_id = login.login_id.getOrElse(-1)
+      if (login_id == -1) {
+          // New User
+          val newLogin_id = scala.util.Random.nextInt(10)
+          newLogin = login.copy(login_id = Some(newLogin_id))
+          userWebHashMap.put(newLogin.login_id.getOrElse(-1), newLogin)
+          return newLogin
+      } else {
+          // error
+          return null
+      }
+  }
+  
+  def getLogins(user_id:Int) :List[WebLogin] = {
+      var userWebHashMap = webHashMap.get(user_id).getOrElse(null)
+      if (userWebHashMap == null) {
+          List()
+      } else {
+          userWebHashMap.values.toList
+      }
+  }
+  
+  def deleteLogin(user_id:Int, login_id:Int) = {
+      var userWebHashMap = webHashMap.get(user_id).getOrElse(null)
+      if (userWebHashMap != null) {
+          userWebHashMap.remove(login_id)
+      }
+  }
+  
+  // Bank Account
+  def createBankAccount(user_id:Int, account:BankAccount): BankAccount = {
+      var userBankHashMap = bankHashMap.get(user_id).getOrElse(null)
+      // Create hash map if necessary
+      if (userBankHashMap == null) {
+          userBankHashMap = HashMap[Int, BankAccount]()
+          bankHashMap.put(user_id, userBankHashMap);
+      }
+      
+      // Validate 
+      var newAccount:BankAccount = null;
+      val ba_id = account.ba_id.getOrElse(-1)
+      if (ba_id == -1) {
+          // New User
+          val newBa_id = scala.util.Random.nextInt(10)
+          newAccount = account.copy(ba_id = Some(newBa_id))
+          userBankHashMap.put(newAccount.ba_id.getOrElse(-1), newAccount)
+          return newAccount
+      } else {
+          // error
+          return null
+      }
+  }
+  
+  def getBankAccounts(user_id:Int) :List[BankAccount] = {
+      var userBankHashMap = bankHashMap.get(user_id).getOrElse(null)
+      if (userBankHashMap == null) {
+          List()
+      } else {
+          userBankHashMap.values.toList
+      }
+  }
+  
+  def deleteBankAccount(user_id:Int, ba_id:Int) = {
+      var userBankHashMap = bankHashMap.get(user_id).getOrElse(null)
+      if (userBankHashMap != null) {
+          userBankHashMap.remove(ba_id)
+      }
+  }
+}
